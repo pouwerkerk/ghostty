b8e2b5aadd5cc0d54e9ed79a6eea3bd6fda0bd87
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2026-02-15 13:06:35 -0800

    Label tests

--- macos/Tests/Splits/SplitTreeTests.swift
+++ macos/Tests/Splits/SplitTreeTests.swift
@@ -2,23 +2,27 @@ import Testing
 @testable import Ghostty
 
 struct SplitTreeTests {
+    /// An empty tree is empty.
     @Test func emptyTreeIsEmpty() {
         let tree = SplitTree<MockView>()
         #expect(tree.isEmpty)
     }
 
+    /// A non-empty tree is not empty.
     @Test func nonEmptyTreeIsNotEmpty() {
         let view1 = MockView()
         let tree = SplitTree<MockView>(view: view1)
         #expect(!tree.isEmpty)
     }
 
+    /// A tree with a single view is not split.
     @Test func isNotSplit() {
         let view1 = MockView()
         let tree = SplitTree<MockView>(view: view1)
         #expect(!tree.isSplit)
     }
 
+    /// A tree with an inserted view is split.
     @Test func isSplit() throws {
         let view1 = MockView()
         let view2 = MockView()
