9b80105588128c9d8f36954f4da586b9a1006f61
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2026-02-15 13:37:56 -0800

    Add tests for contains

--- macos/Tests/Splits/SplitTreeTests.swift
+++ macos/Tests/Splits/SplitTreeTests.swift
@@ -30,4 +30,18 @@ struct SplitTreeTests {
         tree = try tree.inserting(view: view2, at: view1, direction: .right)
         #expect(tree.isSplit)
     }
+
+    /// A tree with a view contains that view.
+    @Test func treeContainsView() {
+        let view = MockView()
+        let tree = SplitTree<MockView>(view: view)
+        #expect(tree.contains(view))
+    }
+
+    /// A tree without a view does not contain that view.
+    @Test func treeDoesNotContainView() {
+        let view = MockView()
+        let tree = SplitTree<MockView>()
+        #expect(!tree.contains(view))
+    }
 }
