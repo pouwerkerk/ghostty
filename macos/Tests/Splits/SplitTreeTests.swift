5916d7eb7c660c4773e4342d8c624b003d12a094
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2026-02-15 20:03:17 +0000

    Add isSplit and isNotSplit tests

--- macos/Tests/Splits/SplitTreeTests.swift
+++ macos/Tests/Splits/SplitTreeTests.swift
@@ -6,4 +6,18 @@ struct SplitTreeTests {
         let tree = SplitTree<MockView>()
         #expect(tree.isEmpty)
     }
+
+    @Test func isNotSplit() {
+        let view1 = MockView()
+        let tree = SplitTree<MockView>(view: view1)
+        #expect(!tree.isSplit)
+    }
+
+    @Test func isSplit() throws {
+        let view1 = MockView()
+        let view2 = MockView()
+        var tree = SplitTree<MockView>(view: view1)
+        tree = try tree.inserting(view: view2, at: view1, direction: .right)
+        #expect(tree.isSplit)
+    }
 }
